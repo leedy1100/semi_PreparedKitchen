461bb8d30c4356edcd9c28479dc23e4727893b04
leedy1100 <dy921119@gmail.com>
2019-08-06 17:06:03 +0900

    ldy

--- PreparedKitchen/src/com/pk/biz/ProductListBiz.java
+++ PreparedKitchen/src/com/pk/biz/ProductListBiz.java
@@ -1,5 +1,18 @@
 package com.pk.biz;
 
+import org.json.simple.JSONArray;
+
+import com.pk.dao.ProductListDao;
+
 public class ProductListBiz {
 
+	ProductListDao productListDao = new ProductListDao();
+	
+	public JSONArray productViewChart() {
+		return productListDao.productViewChart();
+	}
+	
+	public JSONArray productSalesChart() {
+		return productListDao.productSalesChart();
+	}
 }
