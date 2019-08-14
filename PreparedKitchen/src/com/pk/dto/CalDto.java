f5df875958480c93203297f7e5d5dd8a482cbbb2
sincerely008 <sincerely008@gmail.com>
2019-08-14 15:52:02 +0900

    맵퍼 수정 중

--- PreparedKitchen/src/com/pk/dto/CalDto.java
+++ PreparedKitchen/src/com/pk/dto/CalDto.java
@@ -4,42 +4,33 @@ package com.pk.dto;
 
 public class CalDto {
 	
-	private int payment_no;
+	private int calendar_no;
 	private String payment_group;
-	private String id;
-	private int recipe_no;
 	private String recipe_date;
 	private String recipe_name;
+	private int recipe_no;
+	private String id;
 	
 	
-	public CalDto() {
+	public CalDto(int calendar_no, String payment_group, String recipe_date, String recipe_name, int recipe_no,
+			String id) {
 		
-	}
-	
-	
-
-
-	public CalDto(int payment_no, String payment_group, String id, int recipe_no, String recipe_date,
-			String recipe_name) {
-	
-		this.payment_no = payment_no;
+		this.calendar_no = calendar_no;
 		this.payment_group = payment_group;
-		this.id = id;
-		this.recipe_no = recipe_no;
 		this.recipe_date = recipe_date;
 		this.recipe_name = recipe_name;
+		this.recipe_no = recipe_no;
+		this.id = id;
 	}
 
 
-
-
-	public int getPayment_no() {
-		return payment_no;
+	public int getCalendar_no() {
+		return calendar_no;
 	}
 
 
-	public void setPayment_no(int payment_no) {
-		this.payment_no = payment_no;
+	public void setCalendar_no(int calendar_no) {
+		this.calendar_no = calendar_no;
 	}
 
 
@@ -50,26 +41,6 @@ public class CalDto {
 
 	public void setPayment_group(String payment_group) {
 		this.payment_group = payment_group;
-	}
-
-
-	public String getId() {
-		return id;
-	}
-
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
-	public int getRecipe_no() {
-		return recipe_no;
-	}
-
-
-	public void setRecipe_no(int recipe_no) {
-		this.recipe_no = recipe_no;
 	}
 
 
@@ -93,6 +64,26 @@ public class CalDto {
 	}
 
 
+	public int getRecipe_no() {
+		return recipe_no;
+	}
+
+
+	public void setRecipe_no(int recipe_no) {
+		this.recipe_no = recipe_no;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 	
 
 }
